22e6bf07f93a070ee535dbbdcd64e0c47eabdbea
aoki333 <tto.aoki@gmail.com>
2020-09-21 12:14:49 +0900

    error status display

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,31 +4,64 @@ namespace App\Controller;
  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  use Symfony\Component\HttpFoundation\Request;
  use Symfony\Component\HttpFoundation\Response;
- use Symfony\Component\HttpFoundation\RedirectResponse;
  use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
-	/**
-	* @Route("/hello",name="hello")
-	*/
-	public function index(Request $request)
-	{
-		$result='<html><body>';
-		$result.='<h1>Parameter</h1>';
-		$result.='<p>This is index page.</p>';
-		$result.='</body></html>';
-		return new Response($result);
-	}
-	/**
-	* @Route("/other/{domain}",name="other")
-	*/
-	public function other(Request $request,$domain='')
-	{
-		if($domain==''){
-			return $this->redirect('/hello');
-		}else{
-			return new RedirectResponse("http://{$domain}.com");
-		}
-	}
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function index(Request $request)
+    {
+        $content = <<< EOM
+        <html><head><title>Hello</title></head>
+        <body><h1>Hello!</h1>
+        <p>this is Symfony sample page.</p>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+        );        
+        return $response;
+    }
+
+
+    /**
+     * @Route("/notfound", name="notfound")
+     */
+    public function notfound(Request $request)
+    {
+        $content = <<< EOM
+        <html><head><title>ERROR</title></head>
+        <body><h1>ERROR! 404</h1>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_NOT_FOUND,
+            array('content-type' => 'text/html')
+        );
+        return $response;
+    }
+
+
+    /**
+     * @Route("/error", name="error")
+     */
+    public function error(Request $request)
+    {
+        $content = <<< EOM
+        <html><head><title>ERROR</title></head>
+        <body><h1>ERROR! 500</h1>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_INTERNAL_SERVER_ERROR,
+            array('content-type' => 'text/html')
+        );
+        return $response;
+    }
 }
