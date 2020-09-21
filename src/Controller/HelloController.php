6debb78d6057341c09aadaedf339025cfa291b9c
aoki333 <tto.aoki@gmail.com>
2020-09-21 11:08:47 +0900

    redirectMethod and RedirectResponse

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,22 +1,34 @@
 <?php
 
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use Symfony\Component\HttpFoundation\Request;
  use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\HttpFoundation\RedirectResponse;
  use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
 	/**
-	* @Route("/hello/{name}/{pass}",name="hello")
+	* @Route("/hello",name="hello")
 	*/
-	public function index($name='(noname)',$pass='(no password)')
+	public function index(Request $request)
 	{
-		$result='<html><body><ol>';
+		$result='<html><body>';
 		$result.='<h1>Parameter</h1>';
-		$result.='<p>name: '.$name.'</p>';
-		$result.='<p>pass: '.$pass.'</p>';
+		$result.='<p>This is index page.</p>';
 		$result.='</body></html>';
 		return new Response($result);
+	}
+	/**
+	* @Route("/other/{domain}",name="other")
+	*/
+	public function other(Request $request,$domain='')
+	{
+		if($domain==''){
+			return $this->redirect('/hello');
+		}else{
+			return new RedirectResponse("http://{$domain}.com");
+		}
 	}
 }
