6fe60f3f320cdcfb6cbf0e50319214b0043032f9
aoki333 <tto.aoki@gmail.com>
2020-09-21 11:43:22 +0900

    useSerializer XmlEncoder ObjectNormalizer

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  use Symfony\Component\HttpFoundation\Request;
  use Symfony\Component\HttpFoundation\Response;
- use Symfony\Component\HttpFoundation\RedirectResponse;
  use Symfony\Component\Routing\Annotation\Route;
+
+ use Symfony\Component\Serializer\Serializer;
+ use Symfony\Component\Serializer\Encoder\XmlEncoder;
+ use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class HelloController extends AbstractController
 {
@@ -14,21 +17,19 @@ class HelloController extends AbstractController
 	*/
 	public function index(Request $request)
 	{
-		$result='<html><body>';
-		$result.='<h1>Parameter</h1>';
-		$result.='<p>This is index page.</p>';
-		$result.='</body></html>';
-		return new Response($result);
-	}
-	/**
-	* @Route("/other/{domain}",name="other")
-	*/
-	public function other(Request $request,$domain='')
-	{
-		if($domain==''){
-			return $this->redirect('/hello');
-		}else{
-			return new RedirectResponse("http://{$domain}.com");
-		}
+		$encoders=array(new XmlEncoder());
+		$normalizers =array(new ObjectNormalizer());
+		$serializer=new Serializer($normalizers, $encoders);
+
+		$data=array(
+			'name'=>array('first'=>'Hanako','second'=>'Tanaka'),
+			'age'=>29,'mail'=>'hanako@yamada.kun'
+		);
+
+		$response=new Response();
+		$response->headers->set('Content-Type','xml');
+		$result=$serializer->serialize($data,'xml');
+		$response->setContent($result);
+		return $response;
 	}
 }
