d7b20b901596e47ddbab0c55e326081b75a8f6f7
aoki333 <tto.aoki@gmail.com>
2020-09-21 12:35:49 +0900

    上手くいかない

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,34 +1,27 @@
 <?php
-
 namespace App\Controller;
- use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
- use Symfony\Component\HttpFoundation\Request;
- use Symfony\Component\HttpFoundation\Response;
- use Symfony\Component\HttpFoundation\RedirectResponse;
- use Symfony\Component\Routing\Annotation\Route;
+
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class HelloController extends AbstractController
 {
-	/**
-	* @Route("/hello",name="hello")
-	*/
-	public function index(Request $request)
-	{
-		$result='<html><body>';
-		$result.='<h1>Parameter</h1>';
-		$result.='<p>This is index page.</p>';
-		$result.='</body></html>';
-		return new Response($result);
-	}
-	/**
-	* @Route("/other/{domain}",name="other")
-	*/
-	public function other(Request $request,$domain='')
-	{
-		if($domain==''){
-			return $this->redirect('/hello');
-		}else{
-			return new RedirectResponse("http://{$domain}.com");
-		}
-	}
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function index(Request $request, LoggerInterface $logger)
+    {
+        $data = array(
+            'name'=>array('first'=>'Taro','second'=>'Yamada'),
+            'age'=>36, 'mail'=>'taro@yamada.kun'
+        );
+        $logger->info(serialize($data));
+        return new JsonResponse($data);
+    }
 }
