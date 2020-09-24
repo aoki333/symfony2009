20ece53ccad868d5155077ceab0d46d83aa36d13
aoki333 <tto.aoki@gmail.com>
2020-09-24 10:45:26 +0900

    in production

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,34 @@
 
 namespace App\Controller;
  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
- use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\HttpFoundation\Request;
  use Symfony\Component\Routing\Annotation\Route;
+
+ use Symfony\Component\Form\Extension\Core\Type\TextType;
+ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class HelloController extends AbstractController
 {
-	/**
-	* @Route("/hello",name="hello")
-	*/
-	public function index()
-	{
-		return new Response('Hello Symfony!');
-	}
+    /**
+    * @Route("/hello",name="hello")
+    */
+    public function index(Request $request)
+    {
+        $form=$this->createFormBuilder()
+        ->add('input',TextType::class)
+        ->add('save',SubmitType::class,['label'=>'click'])
+        ->getForm();
+
+        if($request->getMethod()=='POSt'){
+            $form->handleRequest($request);
+            $msg='こんにちは'.$form->get('input')->getDate().'さん!';
+        }else{
+            $msg='お名前をどうぞ！';
+        }
+        return $this->render('hello/index.html.twig',[
+            'title'=>'Hello',
+            'message'=>$msg,
+            'form'=>$form->createView(),
+        ])
+    }
 }
