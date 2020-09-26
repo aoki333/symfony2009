62111cf95c88d474823f08fc5d80b653b3b3f87a
aoki333 <tto.aoki@gmail.com>
2020-09-26 11:25:52 +0900

    for

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -13,9 +13,15 @@ class HelloController extends AbstractController
     */
     public function index(Request $request)
     {
+        $data=[
+            array('name'=>'Taro','age'=>37,'mail'=>'taro@yamada'),
+            array('name'=>'Hanako','age'=>24,'mail'=>'hanako@flowe'),
+            array('name'=>'Hanako2','age'=>23,'mail'=>'hanako2@flowe'),
+            array('name'=>'Hanako3','age'=>22,'mail'=>'hanako3@flowe'),
+        ];
        return $this->render('hello/index.html.twig',[
         'title'=>'Hello',
-        'message'=>'これはサンプルのテンプレート計算を行います。',
+        'data'=>$data,
        ]);
     }
 }
